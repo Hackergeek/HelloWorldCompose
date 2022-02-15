1a0abbcb64bb32e167007bc891febcaadc618efa
zhichang chen <chenzhichang@codemao.cn>
2022-02-15 11:05:50 +0800

    9. 增加深色主题预览

--- app/src/main/java/com/skyward/helloworldcompose/MainActivity.kt
+++ app/src/main/java/com/skyward/helloworldcompose/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.os.ConfigurationCompat
 import com.skyward.helloworldcompose.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
